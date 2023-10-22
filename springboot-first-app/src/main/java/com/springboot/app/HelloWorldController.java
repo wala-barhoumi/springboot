78ec06cf015ddc32e11b89450c0db8c36f6e5b55
wala barhoumi <walabarhoumi081@gmail.com>
2023-10-22 14:41:18 +0100

    tp springboot

---
+++ springboot-first-app/src/main/java/com/springboot/app/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.springboot.app;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController {
+    // GET HTTP Method
+// http://localhost:8090/hello-world
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+}
